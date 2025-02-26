27907674ba804ef2d4e4e48c3ae69db354830f3d
ogstevyn <secondsteveoyebamiji@gmail.com>
2025-02-26 05:50:33 +0100

    sign in api completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
-const { signupSchema } = require("../middleware/validator");
+const { jwt } = require("jsonwebtoken");
+const { signupSchema, signinSchema } = require("../middleware/validator");
 const userModel = require("../models/userModel");
-const { doHash } = require("../utils/hashing");
+const { doHash, doHashValidation } = require("../utils/hashing");
 
 exports.signup = async (req, res) => {
   const { email, password } = req.body;
@@ -43,10 +44,50 @@ exports.signup = async (req, res) => {
 exports.signin = async (req, res) => {
   const { email, password } = req.body;
   try {
+    const { error, value } = signinSchema.validate({ email, password });
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+
+    const existingUser = await User.findOne({ email }).select("+password");
+
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User does not exist!" });
+    }
+
+    const result = await doHashValidation(password, existingUser.password);
+    if (!result) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials!" });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: existingUser._id,
+        email: existingUser.email,
+        verified: existingUser.verified,
+      },
+      process.env.TOKEN_SECRET
+    );
+
+    res
+      .cookie("Authorization", "Bearer" + token, {
+        expires: new Date(Date.now + 8 * 3600000),
+        httpOnly: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production",
+      })
+      .json({
+        success: true,
+        token,
+        message: "User logged in successfully!",
+      });
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .json({ success: false, message: "Invalid sign in credentials" });
   }
 };
--- middleware/validator.js
+++ middleware/validator.js
@@ -14,3 +14,18 @@ exports.signupSchema = Joi.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     ),
 });
+
+exports.signinSchema = Joi.object({
+  email: Joi.string()
+    .min(6)
+    .max(60)
+    .required()
+    .email({
+      tlds: { allow: ["net", "com"] },
+    }),
+  password: Joi.string()
+    .required()
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    ),
+});
